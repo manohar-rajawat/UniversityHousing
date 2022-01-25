c11b499d3bde8e85e12e0689f9024e610f7d09ce
Manohar Singh Rajawat <manoharsingh1920@gmail.com>
2022-01-25 02:02:57 -0800

    Inhancement: Dynamic

--- components/Amenity.js
+++ components/Amenity.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const iconColor = "green";
+const iconColorCross = 'tomato';
 const iconSize = 20;
-
 const Row = ({ children }) => (
     <View style={styles.rowContainer}>
         {children}
@@ -14,80 +12,36 @@ const Row = ({ children }) => (
 )
 
 export default Amenity = ({ amenity }) => {
+    const amenityList = ['Gym', 'Swimming', 'Parking', 'Wifi', 'Sports Ground', 'Indoor Games', 'Barbecue', 'Jacuzzi', 'Laundry'].map((element, index) => {
+        const currentElement = amenity[element];
+        if (typeof (currentElement) == 'object')
+            return null;
+        if (currentElement != undefined) {
+            const checkBox = currentElement ? "checkmark-circle" : "close-circle";
+            const iconColor = currentElement ? "green" : "tomato";
+            return (
+                <Row key={index}>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.text}>{element}</Text>
+                    </View>
+                    <View style={styles.icon}>
+                        <Ionicons name={checkBox} size={iconSize} color={iconColor} />
+                    </View>
+                </Row>
+            )
+        }
+
+    })
     return (
         <View style={styles.amanityContainer}>
             <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Gym</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="checkmark-circle" size={iconSize} color={iconColor} />
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'green', paddingVertical: 5, borderRadius: 20, }}>
+                    <Text style={{ fontSize: 20, fontStyle: "italic", fontWeight: 'bold', color: 'white', letterSpacing: 1, }}>Amenities</Text>
                 </View>
             </Row>
-            <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Swimming</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="checkmark-circle" size={iconSize} color={iconColor} />
-                </View>
-            </Row>
-            <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Parking</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="close-circle" size={iconSize} color='red' />
-                </View>
-            </Row>
-            <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Wifi</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="checkmark-circle" size={iconSize} color={iconColor} />
-                </View>
-            </Row>
-            <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Sport Ground</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="checkmark-circle" size={iconSize} color={iconColor} />
-                </View>
-            </Row>
-            <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Indoor Games</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="checkmark-circle" size={iconSize} color={iconColor} />
-                </View>
-            </Row>
-            <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Barbecue</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="close-circle" size={iconSize} color='red' />
-                </View>
-            </Row>
-            <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Jacuzzi</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="close-circle" size={iconSize} color='red' />
-                </View>
-            </Row>
-            <Row>
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>Laundry</Text>
-                </View>
-                <View style={styles.icon}>
-                    <Ionicons name="checkmark-circle" size={iconSize} color={iconColor} />
-                </View>
-            </Row>
+            {
+                amenityList
+            }
         </View>
     )
 }
